82c2b97de1407d25bd60db2aa7a8aec5e6347942
Balazsnemes1016 <nemesbazsu@gmail.com>
2025-05-11 01:52:01 +0200

    A szamitasinput modellneinek megadasa

--- BACKEND/VILLANYSZAM_BACKEND/Models/SzamitasInput.cs
+++ BACKEND/VILLANYSZAM_BACKEND/Models/SzamitasInput.cs
@@ -1,6 +1,8 @@
-﻿namespace VILLANYSZAM_BACKEND.Models
+﻿namespace VillanyszamlaBackend.Models
 {
     public class SzamitasInput
     {
+        public double UnitPrice { get; set; }
+        public string Matrix { get; set; }
     }
 }
