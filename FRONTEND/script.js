0d3a0901e1ae1d44a147b0459e37ae0f8cb4cfca
Balazsnemes1016 <nemesbazsu@gmail.com>
2025-05-11 01:25:54 +0200

    A gomb analizalasa es az input mezok ertekeinek beolvasasa(price,matrix)

---
+++ FRONTEND/script.js
@@ -0,0 +1,5 @@
+document.getElementById("calculateBtn").addEventListener("click", async () => { //ez figyeli magat a gombot 
+    const price = parseFloat(document.getElementById("price").value);
+    const matrixText = document.getElementById("matrix").value;
+  
+    
