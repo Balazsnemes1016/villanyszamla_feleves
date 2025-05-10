e7e7b0bf93c94d474568934f12c0e47b8fecc72a
Balazsnemes1016 <nemesbazsu@gmail.com>
2025-05-11 01:53:32 +0200

    szamitasvalasz, ez maga az osztaly amit visszakuldunk json formaba

--- BACKEND/VILLANYSZAM_BACKEND/Models/SzamitasValasz.cs
+++ BACKEND/VILLANYSZAM_BACKEND/Models/SzamitasValasz.cs
@@ -1,6 +1,11 @@
-﻿namespace VILLANYSZAM_BACKEND.Models
+﻿using System.Collections.Generic;
+
+namespace VillanyszamlaBackend.Models
 {
     public class SzamitasValasz
     {
+        public Dictionary<string, List<double>> HaviDijak { get; set; }
+        public Dictionary<string, double> EvesDijak { get; set; }
+        public List<int> KedvezmenyesEvek { get; set; }
     }
 }
